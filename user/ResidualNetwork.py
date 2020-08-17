b2b902889e66c5cd03fc3897eed1e952f6e00c90
shjo0501 <shjo0501@gmail.com>
2020-08-17 16:04:46 +0900

    add ResidualNetwork using MNIST

---
+++ user/ResidualNetwork.py
@@ -0,0 +1,136 @@
+import tensorflow as tf
+import numpy as np
+
+EPOCHS = 10
+
+class ResidualUnit(tf.keras.Model):
+    def __init__(self, filter_in, filter_out, kernel_size):
+        super(ResidualUnit, self).__init__()
+        self.bn1 = tf.keras.layers.BatchNormalization()
+        self.conv1 = tf.keras.layers.Conv2D(filter_out, kernel_size, padding = 'same')
+
+        self.bn2 = tf.keras.layers.BatchNormalization()
+        self.conv2 = tf.keras.layers.Conv2D(filter_out, kernel_size, padding = 'same')
+
+        if filter_in == filter_out:
+            self.identity = lambda x : x 
+        else:
+            self.identity = tf.keras.layers.Conv2D(filter_out, (1, 1), padding = 'same')
+
+    def call(self, x, training=False, mask=None):
+        h = self.bn1(x, training = training)
+        h = tf.nn.relu(h)
+        h = self.conv1(h)
+
+        h = self.bn2(h, training = training)
+        h = tf.nn.relu(h)
+        h = self.conv2(h)
+
+        return self.identity(x) + h
+
+class ResnetLayer(tf.keras.Model):
+    def __init__(self, filter_in, filters, kernel_size):
+        super(ResnetLayer, self).__init__()
+        self.sequence = list()
+        for filterIn, filterOut in zip([filter_in] + list(filters), filters):
+            self.sequence.append(ResidualUnit(filterIn, filterOut, kernel_size))
+
+    def call(self, x, training=False, mask=None):
+        for unit in self.sequence:
+            x = unit(x, training = training)
+        return x
+
+class ResNet(tf.keras.Model):
+    def __init__(self):
+        super(ResNet, self).__init__()
+        self.conv1 = tf.keras.layers.Conv2D(8, (3, 3), padding = 'same', activation = 'relu') # 28 x 28 x 8
+
+        self.res1 = ResnetLayer(8, (16, 16), (3, 3))  # 28 x 28 x 16
+        self.pool1 = tf.keras.layers.MaxPool2D((2, 2))
+
+        self.res2 = ResnetLayer(16, (32, 32), (3, 3)) # 14 x 14 x 32
+        self.pool2 = tf.keras.layers.MaxPool2D((2, 2))
+
+        self.res3 = ResnetLayer(32, (64, 64), (3, 3)) # 7 x 7 x 64
+
+        self.flatten = tf.keras.layers.Flatten()
+        self.dense1 = tf.keras.layers.Dense(128, activation = 'relu')
+        self.dense2 = tf.keras.layers.Dense(10, activation = 'softmax')
+
+    def call(self, x, training=False, mask=None):
+        x = self.conv1(x)
+
+        x = self.res1(x, training = training)
+        x = self.pool1(x)
+        x = self.res2(x, training = training)
+        x = self.pool2(x)
+        x = self.res3(x, training = training)
+
+        x = self.flatten(x)
+        x = self.dense1(x)
+        return self.dense2(x)
+
+# Implement training loop
+@tf.function
+def train_step(model, images, labels, loss_object, optimizer, train_loss, train_accuracy):
+    with tf.GradientTape() as tape:
+        predictions = model(images)
+        loss = loss_object(labels, predictions)
+    gradients = tape.gradient(loss, model.trainable_variables)
+
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+    train_loss(loss)
+    train_accuracy(labels, predictions)
+
+# Implement algorithm test
+@tf.function
+def test_step(model, images, labels, loss_object, test_loss, test_accuracy):
+    predictions = model(images)
+
+    t_loss = loss_object(labels, predictions)
+    test_loss(t_loss)
+    test_accuracy(labels, predictions)
+
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+x_train = x_train[..., tf.newaxis].astype(np.float32)
+x_test = x_test[..., tf.newaxis].astype(np.float32)
+
+train_ds = tf.data.Dataset.from_tensor_slices((x_train, y_train)).shuffle(10000).batch(32)
+test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(32)
+
+# Create model
+model = ResNet()
+
+# Define loss and optimizer
+loss_object = tf.keras.losses.SparseCategoricalCrossentropy()
+optimizer = tf.keras.optimizers.Adam()
+
+# Define performance metrics
+train_loss = tf.keras.metrics.Mean(name='train_loss')
+train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy')
+
+test_loss = tf.keras.metrics.Mean(name='test_loss')
+test_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='test_accuracy')
+
+for epoch in range(EPOCHS):
+    for images, labels in train_ds:
+        train_step(model, images, labels, loss_object, optimizer, train_loss, train_accuracy)
+
+    for test_images, test_labels in test_ds:
+        test_step(model, test_images, test_labels, loss_object, test_loss, test_accuracy)
+
+    template = 'Epoch {}, Loss: {}, Accuracy: {}, Test Loss: {}, Test Accuracy: {}'
+    print(template.format(epoch + 1,
+                          train_loss.result(),
+                          train_accuracy.result() * 100,
+                          test_loss.result(),
+                          test_accuracy.result() * 100))
+    train_loss.reset_states()
+    train_accuracy.reset_states()
+    test_loss.reset_states()
+    test_accuracy.reset_states()
+
